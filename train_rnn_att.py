982f4bbb5efec131f88252042930a1456ca87d09
lvyilin <lvyilin48@foxmail.com>
2018-08-13 15:07:43 +0800

    add: GRU+ATT

---
+++ train_rnn_att.py
@@ -0,0 +1,114 @@
+import time
+import os
+import numpy as np
+import mxnet as mx
+from mxnet import gluon, init, autograd, nd
+from mxnet.gluon import loss as gloss, nn, rnn
+
+SENTENCE_DIMENSION = 100
+DIMENSION = SENTENCE_DIMENSION
+FIXED_WORD_LENGTH = 60
+ADAPTIVE_LEARNING_RATE = True
+
+input_train = np.load('data_train_rnn.npy')
+input_test = np.load('data_test_rnn.npy')
+x_train = input_train[:, 1:].reshape((input_train.shape[0], FIXED_WORD_LENGTH, DIMENSION))
+# x_train = np.expand_dims(x_train, axis=1)
+y_train = input_train[:, 0]
+print(x_train.shape)
+print(y_train.shape)
+x_test = input_test[:, 1:].reshape((input_test.shape[0], FIXED_WORD_LENGTH, DIMENSION))
+# x_test = np.expand_dims(x_test, axis=1)
+y_test = input_test[:, 0]
+print(x_test.shape)
+print(y_test.shape)
+
+x_train = x_train.astype(np.float32)
+y_train = y_train.astype(np.float32)
+x_test = x_test.astype(np.float32)
+y_test = y_test.astype(np.float32)
+print(x_train.shape, x_test.shape)
+
+
+class Network(nn.Block):
+    def __init__(self, prefix=None, params=None):
+        super().__init__(prefix, params)
+
+        self.gru = rnn.GRU(64, num_layers=1, bidirectional=True, dropout=0.2)
+        self.att = nn.Sequential()
+        self.att.add(nn.Dense(1, flatten=False,
+                              activation="sigmoid"))
+        self.att_out = nn.Sequential()
+        self.att_out.add(nn.Dense(100, activation="relu"))
+
+        self.output = nn.Sequential()
+        self.output.add(nn.Dropout(0.5))
+        self.output.add(nn.Dense(6))
+
+    def forward(self, input_data):
+        x = nd.transpose(input_data, axes=(1, 0, 2))
+        h = nd.transpose(self.gru(x), axes=(1, 0, 2)) #(m,60,100)
+        h = nd.tanh(h)
+        g = self.att(h) #(m,60,1)
+        g=nd.softmax(g, axis=1)
+        gt = nd.transpose(g, axes=(0, 2, 1)) #(m,1,60)
+        n = nd.batch_dot(gt, h)
+        y = self.att_out(n)
+        return self.output(y)
+
+
+net = Network()
+net.initialize(init=init.Xavier())
+print(net)
+
+batch_size = 128
+num_epochs = 100
+decay_rate = 0.1
+gap = 25
+loss = gloss.SoftmaxCrossEntropyLoss()
+# trainer = gluon.Trainer(net.collect_params(), 'AdaDelta', {'rho': 0.95, 'epsilon': 1e-6, 'wd': 0.01})
+# trainer = gluon.Trainer(net.collect_params(), 'sgd', {'learning_rate': .01})
+if ADAPTIVE_LEARNING_RATE:
+    trainer = gluon.Trainer(net.collect_params(), 'adam', {'learning_rate': 0.01})
+else:
+    trainer = gluon.Trainer(net.collect_params(), 'adam', {'learning_rate': 0.0001})
+
+train_data = gluon.data.DataLoader(gluon.data.ArrayDataset(x_train, y_train), batch_size, shuffle=True)
+test_data = gluon.data.DataLoader(gluon.data.ArrayDataset(x_test, y_test), batch_size, shuffle=False)
+
+
+def accuracy(y_hat, y):
+    return (y_hat.argmax(axis=1) == y.astype('float32')).mean().asscalar()
+
+
+def evaluate_accuracy(data_iter, net):
+    acc = 0
+    for X, y in data_iter:
+        acc += accuracy(net(X), y)
+    return acc / len(data_iter)
+
+
+def train(net, train_iter, test_iter, loss, num_epochs, batch_size, trainer):
+    for epoch in range(1, num_epochs + 1):
+        train_l_sum = 0
+        train_acc_sum = 0
+        if ADAPTIVE_LEARNING_RATE and epoch % gap == 0:
+            trainer.set_learning_rate(trainer.learning_rate * decay_rate)
+            print("learning_rate decay: %f" % trainer.learning_rate)
+        start = time.time()
+        for X, y in train_iter:
+            # print(X.shape)
+            with autograd.record():
+                y_hat = net(X)
+                l = loss(y_hat, y)
+            l.backward()
+            trainer.step(batch_size)
+            train_l_sum += l.mean().asscalar()
+            train_acc_sum += accuracy(y_hat, y)
+        test_acc = evaluate_accuracy(test_iter, net)
+        print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f time %.1f sec'
+              % (epoch, train_l_sum / len(train_iter),
+                 train_acc_sum / len(train_iter), test_acc, time.time() - start))
+
+
+train(net, train_data, test_data, loss, num_epochs, batch_size, trainer)
