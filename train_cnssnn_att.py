a44ceb5f00348a7a6022fa8bb37b0363a25c0394
lvyilin <lvyilin48@foxmail.com>
2018-08-14 17:44:00 +0800

    update: cnssnn with sentence attention

---
+++ train_cnssnn_att.py
@@ -0,0 +1,161 @@
+import time
+
+import numpy as np
+import mxnet as mx
+from mxnet import gluon, init, autograd, nd
+from mxnet.gluon import loss as gloss, nn, rnn
+
+WORD_DIMENSION = 100
+POS_DIMENSION = 5
+DIMENSION = WORD_DIMENSION + 2 * POS_DIMENSION
+FIXED_WORD_LENGTH = 60
+MAX_ENTITY_DEGREE = 50
+ENTITY_DEGREE = MAX_ENTITY_DEGREE + 1
+MASK_LENGTH = ENTITY_DEGREE
+ENTITY_EDGE_VEC_LENGTH = ENTITY_DEGREE * (WORD_DIMENSION * 2)
+VEC_LENGTH = DIMENSION * FIXED_WORD_LENGTH + ENTITY_EDGE_VEC_LENGTH * 2
+ADAPTIVE_LEARNING_RATE = True
+
+input_train = np.load('data_train_cnssnn.npy')
+input_test = np.load('data_test_cnssnn.npy')
+
+x_train = input_train[:, 3:]
+y_train = input_train[:, 0]
+print(x_train.shape)
+print(y_train.shape)
+x_test = input_test[:, 3:]
+y_test = input_test[:, 0]
+print(x_test.shape)
+print(y_test.shape)
+
+x_train = x_train.astype(np.float32)
+y_train = y_train.astype(np.float32)
+x_test = x_test.astype(np.float32)
+y_test = y_test.astype(np.float32)
+print(x_train.shape, x_test.shape)
+
+batch_size = 128
+num_epochs = 100
+loss = gloss.SoftmaxCrossEntropyLoss()
+
+train_data = gluon.data.DataLoader(gluon.data.ArrayDataset(x_train, y_train), batch_size, shuffle=True)
+test_data = gluon.data.DataLoader(gluon.data.ArrayDataset(x_test, y_test), batch_size, shuffle=False)
+
+
+def accuracy(y_hat, y):
+    return (y_hat.argmax(axis=1) == y).mean().asscalar()
+
+
+def evaluate_accuracy(data_iter, net):
+    acc = 0
+    for X, y in data_iter:
+        acc += accuracy(net(X), y)
+    return acc / len(data_iter)
+
+
+decay_rate = 0.1
+epochs = 200
+gap = 50
+
+
+def train(net, train_iter, test_iter):
+    for epoch in range(1, epochs + 1):
+        train_loss_sum = 0
+        train_acc_sum = 0
+        start = time.time()
+        if ADAPTIVE_LEARNING_RATE and epoch % gap == 0:
+            trainer.set_learning_rate(trainer.learning_rate * decay_rate)
+            print("learning_rate decay: %f" % trainer.learning_rate)
+        for X, y in train_iter:
+            with autograd.record():
+                y_hat = net(X)
+                lss = loss(y_hat, y)
+            lss.backward()
+            trainer.step(batch_size, ignore_stale_grad=True)
+            train_loss_sum += lss.mean().asscalar()
+            train_acc_sum += accuracy(y_hat, y)
+        test_acc = evaluate_accuracy(test_iter, net)
+        print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f time %.1f sec'
+              % (epoch, train_loss_sum / len(train_iter),
+                 train_acc_sum / len(train_iter), test_acc, time.time() - start))
+
+
+class Network(nn.Block):
+    def __init__(self, **kwargs):
+        super(Network, self).__init__(**kwargs)
+        with self.name_scope():
+            self.gru = rnn.GRU(100, num_layers=1, bidirectional=True)
+            self.att = nn.Sequential()
+            self.att.add(nn.Dense(1, flatten=False,
+                                  activation="sigmoid"))
+            self.att_out = nn.Sequential()
+            self.att_out.add(nn.Dense(200, activation="relu"))
+
+            self.center_att = nn.Sequential()
+            self.center_att.add(nn.Dense(1, in_units=200, flatten=False,
+                                         activation="sigmoid"))
+            self.center_out = nn.Sequential()
+            self.center_out.add(nn.Dense(200, activation="relu"))
+            self.output = nn.Sequential()
+            self.output.add(nn.Dropout(0.5))
+            self.output.add(nn.Dense(6))
+
+    def forward(self, input_data):
+        e1_vec_start = FIXED_WORD_LENGTH * DIMENSION
+        x = input_data[:, :e1_vec_start].reshape(
+            (input_data.shape[0], FIXED_WORD_LENGTH, DIMENSION))  # (m, 60, 110)
+
+        e1neimask = input_data[:, e1_vec_start:e1_vec_start + MASK_LENGTH]  # (m, 51)
+        e1edge = input_data[:, e1_vec_start + MASK_LENGTH:e1_vec_start + MASK_LENGTH + ENTITY_EDGE_VEC_LENGTH].reshape(
+            (input_data.shape[0], ENTITY_DEGREE, WORD_DIMENSION * 2))  # (m, 51, 200)
+        e1neigh = e1edge[:, :, :WORD_DIMENSION]
+
+        e2_vec_start = e1_vec_start + MASK_LENGTH + ENTITY_EDGE_VEC_LENGTH
+        e2neimask = input_data[:, e2_vec_start:e2_vec_start + MASK_LENGTH]  # (m, 51)
+        e2edge = input_data[:, e2_vec_start + MASK_LENGTH:e2_vec_start + MASK_LENGTH + ENTITY_EDGE_VEC_LENGTH].reshape(
+            (input_data.shape[0], ENTITY_DEGREE, WORD_DIMENSION * 2))  # (m, 51,200)
+        e2neigh = e2edge[:, :, :WORD_DIMENSION]
+
+        x = nd.transpose(x, axes=(1, 0, 2))
+        h = nd.transpose(self.gru(x), axes=(1, 0, 2))
+        h = nd.tanh(h)
+        g = self.att(h)
+        g = nd.softmax(g, axis=1)
+        gt = nd.transpose(g, axes=(0, 2, 1))  # (m,1,60)
+        n = nd.batch_dot(gt, h)
+        y1 = self.att_out(n)
+
+        att = self.center_att
+        e1edge = nd.tanh(e1edge)
+        e1g = att(e1edge)  # (m,51,1)
+        e1g = e1g * e1neimask.expand_dims(2)
+        e1g = nd.softmax(e1g, axis=1)
+        e1gt = nd.transpose(e1g, axes=(0, 2, 1))  # (m,1,151)
+        e1n = nd.batch_dot(e1gt, e1neigh)  # (m,1,100)
+        e1n = e1n.reshape((e1n.shape[0], 100))  # (m,100)
+
+        e2edge = nd.tanh(e2edge)
+        e2g = att(e2edge)  # (m,51,1)
+        e2g = e2g * e2neimask.expand_dims(2)
+        e2g = nd.softmax(e2g, axis=1)
+        e2gt = nd.transpose(e2g, axes=(0, 2, 1))  # (m,1,151)
+        e2n = nd.batch_dot(e2gt, e2neigh)  # (m,1,100)
+        e2n = e2n.reshape((e2n.shape[0], 100))  # (m,100)
+
+        center_y = nd.concat(e1n, e2n, dim=1)  # (m,200)
+        center_out = self.center_out
+        center_y = center_out(center_y)
+
+        out = self.output
+        y4 = nd.concat(y1, center_y, dim=1)
+        y5 = out(y4)
+        return y5
+
+
+net = Network()
+net.initialize()
+if ADAPTIVE_LEARNING_RATE:
+    trainer = gluon.Trainer(net.collect_params(), 'adam', {'beta1': 0.9, 'beta2': 0.99, 'learning_rate': 1e-2})
+else:
+    trainer = gluon.Trainer(net.collect_params(), 'adam', {'learning_rate': 0.0001})
+train(net, train_data, test_data)
